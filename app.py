fc9ebafb1794c2ec1a98b0ca192c37e3e99e93ad
Fionagu <fionagu531@163.com>
2020-03-19 16:01:30 +0800

    simple get and jsonify

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
+tasks = [{
+    'id': 1,
+    'title': u'Buy groceries',
+    'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+    'done': False
+}, {
+    'id': 2,
+    'title': u'Learn Python',
+    'description': u'Need to find a good python tutorail',
+    'done': False
+}]
 
 @app.route('/')
 def index():
     return 'Hello'
 
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
