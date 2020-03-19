cab427edf82c809c1a164303bf130df38d3058f3
Fionagu <fionagu531@163.com>
2020-03-19 17:29:47 +0800

    basic auth

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+
+auth = HTTPBasicAuth()
 
 tasks = [{
     'id': 1,
@@ -21,6 +24,7 @@ def index():
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
     # return jsonify({'tasks': tasks})
     return jsonify({'tasks': list(map(make_public_task, tasks))})
@@ -85,15 +89,28 @@ def not_found(error):
     return make_response(jsonify({'Error': 'Not Found'}), 404)
 
 
-
 def make_public_task(task):
     new_task = {}
     for field in task:
-        if field=='id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external = True)
+        if field == 'id':
+            new_task['uri'] = url_for('get_task',
+                                      task_id=task['id'],
+                                      _external=True)
         else:
-            new_task[field]=task[field]
+            new_task[field] = task[field]
     return new_task
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'ok':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
 if __name__ == '__main__':
