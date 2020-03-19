3af80d209955205fe41da9e28d7a9a215328c1b4
Fionagu <fionagu531@163.com>
2020-03-19 16:21:39 +0800

    create and update task

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify, abort, make_response
-
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -15,6 +14,7 @@ tasks = [{
     'done': False
 }]
 
+
 @app.route('/')
 def index():
     return 'Hello'
@@ -27,15 +27,52 @@ def get_tasks():
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    task = list(filter(lambda t: t['id']== task_id, tasks))
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
     if len(task) == 0:
         abort(404)
-    return jsonify({'task':task})
+    return jsonify({'task': task})
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ''),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'tasks': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = list(filter(lambda t: t['id']==task_id, tasks))
+    if len(task)==0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' not in request.json and type(request.json['title'])!= unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    
+    task[0]['title']=request.json.get('title', task[0]['title'])
+    task[0]['description']=request.json.get('description', task[0]['description'])
+    task[0]['done']=request.json.get('done', task[0]['done'])
+
+    return jsonify({'task':task[0]})
+
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'Error':'Not Found'}), 404)
+    return make_response(jsonify({'Error': 'Not Found'}), 404)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
